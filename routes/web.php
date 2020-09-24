3f9d7b617c41f351da5d47b1c0d3f3a30996a2dd
evenvagen <59914225+evenvagen@users.noreply.github.com>
2020-09-24 15:25:46 +0200

    refactor

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('', function () {
-    return view('contacts/create');
-});
-
 Route::apiResource('contacts', ContactController::class);
