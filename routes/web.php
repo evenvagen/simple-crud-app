99191672568c75d2643d34c0115826796f8c5daa
Even Vågen <evenvagen@Evens-Air.home>
2020-09-24 19:48:09 +0200

    cache routene når jeg oppdaterer

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('contacts/index');
+});
+
 Route::resource('contacts', ContactController::class);
