874493fdfb7237fa3c6e36f7bc1f8e5df9fcbb64
evenvagen <59914225+evenvagen@users.noreply.github.com>
2020-09-24 15:28:43 +0200

    ja

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('contacts', ContactController::class);
+Route::resource('contacts', ContactController::class);
