4f345ba645fffc31f4cffe549312d377a4b70e71
Even Vågen <even@Evens-MBP.home>
2020-09-30 17:37:17 +0200

    styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Oh home</title>
+    <link href="{{ asset('css/grid.css') }}" rel="stylesheet" type="text/css">
+    <title>Main-page</title>
+
+
+    <style>
+        h1{
+            color:green;
+        }
+    </style>
 </head>
 <body>
 
