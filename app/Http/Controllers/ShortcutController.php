a85694501fb11ca53e80e0370b7fcf88c69eb55e
evenvagen <evenvagen@gmail.com>
2020-10-01 07:24:13 +0200

    ;

--- app/Http/Controllers/ShortcutController.php
+++ app/Http/Controllers/ShortcutController.php
@@ -8,11 +8,13 @@ use Illuminate\Http\Request;
 class ShortcutController extends Controller
 {
 
-    public function index(){
-        return view('shortcuts/index')
+    public function index()
+    {
+        return view('shortcuts/index');
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $shortcut =  new Shortcut([
                 'shortcut'=>$request->get('shortcut'),
                 'task'=>$request->get('task'),
