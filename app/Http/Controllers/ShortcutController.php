f5a5e7a5b0a241a93fd08115c5373dff817abfb4
evenvagen <evenvagen@gmail.com>
2020-10-01 07:20:26 +0200

    test

--- app/Http/Controllers/ShortcutController.php
+++ app/Http/Controllers/ShortcutController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Shortcut;
 use Illuminate\Http\Request;
 
 class ShortcutController extends Controller
 {
-    //
+
+    public function index(){
+        return view('shortcuts/index')
+    }
+
+    public function store(Request $request){
+        $shortcut =  new Shortcut([
+                'shortcut'=>$request->get('shortcut'),
+                'task'=>$request->get('task'),
+                'program'=>$request->get('program')
+            ]);
+
+        $shortcut->save();
+    }
 }
